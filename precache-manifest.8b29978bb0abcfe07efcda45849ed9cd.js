33952abc56dfea9e09fc721f4387703eb2decb31
MB2M <damsbooster@gmail.com>
2021-11-29 11:36:24 +0100

    Updates

---
+++ precache-manifest.8b29978bb0abcfe07efcda45849ed9cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317da9d7e71fffbe1a4e8e9e9f27e8f7",
+    "url": "/testBox/index.html"
+  },
+  {
+    "revision": "f33214a84740d247eb83",
+    "url": "/testBox/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b85b35bcdf118c61287c",
+    "url": "/testBox/static/js/2.72b5c483.chunk.js"
+  },
+  {
+    "revision": "f33214a84740d247eb83",
+    "url": "/testBox/static/js/main.f4ee2f33.chunk.js"
+  },
+  {
+    "revision": "ec92ea82fb35fff4ead4",
+    "url": "/testBox/static/js/runtime-main.81da20fa.js"
+  }
+]);
